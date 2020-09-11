d823592c37c63fc1deeceb93e950fb5d594b6233
Vitor Hugo <vitorhugo.pitolsilva@gmail.com>
2020-09-11 18:58:12 -0400

    Add consuming-api

---
+++ code-drops/consuming-api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/users');
+
+    return res.json({ users: data });
+  } catch (error) {
+    return res.json({ error });
+  }
+});
+
+app.listen(4567);
