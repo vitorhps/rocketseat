4ab1cee5eb9fa6a86ec25cc19be8febcb96d9150
Vitor Hugo <vitorhugo.pitolsilva@gmail.com>
2020-09-17 22:47:28 -0400

    Add sql-nodejs-sequelize

---
+++ code-drops/sql-nodejs-sequelize/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = require('./routes');
+
+require('./database');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const port = 3333;
+app.listen(port, () => console.log(`Server running in http://localhost:${port}`));
